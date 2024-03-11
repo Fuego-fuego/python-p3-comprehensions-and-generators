0219b8d128a681fc0b656bd997e58e23611b9483
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-03-11 10:16:21 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -9,14 +9,4 @@ def make_exclamation(sentence_list):
     return add_exclamations
 
 
-# squared_minus_one = [(n**2) - 1 for n in range(1,11)]
 
-# print(squared_minus_one)
-
-# filtered_list = [n for n in squared_minus_one if (n%3==0)== True]
-
-# print(filtered_list)
-
-# one_to_three = range(1,4)
-
-# print(one_to_three)
